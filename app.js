f38dc236e236599413e77efa3175bda0036f072c
James <jamesvo801@gmail.com>
2021-05-29 07:05:01 -0700

    Added search bar functionality
    -Implemented the search bar to make it be able to find different types
    of anime

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ app.use('/styles', express.static(path.join(__dirname, 'public')))
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+app.get('/', async (req,res) =>{
+	try{
+		res.render('homepage');
+	}catch (e) {
+		console.log(e);
+	}
+})
+
 app.get('/topanimes/:page', async (req, res) => {
 	try{
 		const page = parseInt(req.params.page);
@@ -34,39 +42,43 @@ app.get('/topanimes/:page', async (req, res) => {
 });
 
 app.get('/topmangas/:page', async (req, res) => {
-	const page = parseInt(req.params.page);
-	const topMangas = await fetch(`${anime_url}/top/manga/${page}/manga`)
-		.then(res => res.json())
-		.then(data => data.top) //this grabs all the data
+	try{
+		const page = parseInt(req.params.page);
+		const topMangas = await fetch(`${anime_url}/top/manga/${page}/manga`)
+			.then(res => res.json())
+			.then(data => data.top) //this grabs all the data
 
-	console.log(topMangas);
-	res.render('topManga', {topMangas: topMangas, page: page})
+		res.render('topManga', {topMangas: topMangas, page: page})
+	}catch (e) {
+		console.log(e);
+	}
+
 });
 
 app.get('/topupcominganime/:page', async (req, res) => {
-	const page = parseInt(req.params.page);
-	const topUpcomingAnime = await fetch(`${anime_url}/top/anime/${page}/upcoming`)
-		.then(res => res.json())
-		.then(data => data.top) //This grabs all the data
+	try{
+		const page = parseInt(req.params.page);
+		const topUpcomingAnime = await fetch(`${anime_url}/top/anime/${page}/upcoming`)
+			.then(res => res.json())
+			.then(data => data.top) //This grabs all the data
 
-	res.render('topUpcomingAnime', {topUpcomingAnime: topUpcomingAnime, page: page})
+		res.render('topUpcomingAnime', {topUpcomingAnime: topUpcomingAnime, page: page})
+	}catch (e) {
+		console.log(e);
+	}
+
 });
 
 //obtains the top results data for the anime
 app.get("/anime/:anime/:page", async (req, res) => {
 	try{
 		const query = req.params.anime;
-		const page = req.params.page;
-		let animeResults = await fetch(`${anime_url}/search/anime?q=${query}&order_by=title&sort=asc`)
+		const page = parseInt(req.params.page);
+		let animeResults = await fetch(`${anime_url}/search/anime?q=${query}&order_by=title&sort=asc&page=${page}`)
 			.then(res => res.json())
 			.then(data => data.results) //not top because the array is called results
-		if(page < (animeResults.length/10)){ //if the page number is less than the number of entries divided by 10
-			animeResults = animeResults.slice((page-1)*10 , page*10); //this gets only 10 of the anime, 0-10 10-20 based on that algorithm
-		}else{
-			//throw an error if the page is greater
-		}
-		// console.log(animeResults)
-		res.render('animePage', {animeResults})
+
+		res.render('searchAnime', {animeResults: animeResults, page: page, search: query})
 	}catch(e){
 		// console.log("Inside the /anime/:anime/:page");
 		console.log(e);
